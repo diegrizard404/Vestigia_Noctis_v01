a735d21345fb18afbde51d3c46b420772757e1d1
4Amelie <pulpleitor121@hotmail.com>
2024-05-08 12:04:07 -0600

    v4

--- Vestigia Noctis/Assets/Scripts/BookPageManager.cs
+++ Vestigia Noctis/Assets/Scripts/BookPageManager.cs
@@ -6,28 +6,76 @@ public class BookPageManager : MonoBehaviour
 {
     
 
-    [SerializeField] private GameObject page0;
+    
     [SerializeField] private GameObject page1;
     [SerializeField] private GameObject page2;
     [SerializeField] private GameObject page3;
     [SerializeField] private GameObject page4;
+    [SerializeField] private GameObject page5;
 
-    private int list;
+    private int paginaActual = 1;
+   
 
-    public List<GameObject> pages = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        pages.Add(page0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (paginaActual == 1)
+        {
+            page1.SetActive(true);
+            page2.SetActive(false);
+
+        }
+        else if (paginaActual == 2)
+        {
+            page2.SetActive(true);
+            page1.SetActive(false);
+            page3.SetActive(false);
+        }
+        else if (paginaActual == 3)
+        {
+            page3.SetActive(true);
+            page2.SetActive(false);
+            page4.SetActive(false);
+
+        }
+        else if (paginaActual == 4)
+        {
+            page4.SetActive(true);
+            page3.SetActive(false);
+            page5.SetActive(false);
+        }
+        else if (paginaActual == 5)
+        {
+            page5.SetActive(true);
+            page4.SetActive(false);
+            
+        }
+
+        if (paginaActual <= 0)
+            paginaActual = 1;
+        else if (paginaActual >= 6)
+            paginaActual = 5;
+
     }
 
+
+    public void NextPage()
+    {
+        paginaActual++;
+    }
+
+    public void PrevPage() 
+    { 
+        paginaActual--;
+    }
+    /*
     public void ListaDeHojas()
     {
         pages.Add(page1);
@@ -53,4 +101,5 @@ public class BookPageManager : MonoBehaviour
             
         }
     }
+    */
 }
